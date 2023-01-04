30fd4378109d7e85bf7ca87b483120a9d0f61d2b
Sebastian Beltran <bjohansebas@gmail.com>
2023-01-04 18:27:26 -0500

    feat(Root): Add api request for login and signup and verifications of form fields

--- web/src/routes/Login/LoginPage.jsx
+++ web/src/routes/Login/LoginPage.jsx
@@ -1,14 +1,65 @@
 import { Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, Toolbar, Typography, Box, TextField } from '@mui/material'
 import { Visibility, VisibilityOff } from '@mui/icons-material'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from '../../context/authContext'
+import axios from '../../api/axios'
 
 function LoginPage () {
+  const navigate = useNavigate()
+  const { setAuth } = useAuth()
+
+  const [email, setEmail] = useState('')
+  const [pwd, setPwd] = useState('')
+
+  const [errMsg, setErrMsg] = useState('')
+  const [success, setSuccess] = useState(false)
+
   const [showPassword, setShowPassword] = useState(false)
+
+  useEffect(() => {
+    setErrMsg('')
+  }, [email, pwd])
+
   const handleClickShowPassword = () => setShowPassword((show) => !show)
   const handleMouseDownPassword = (event) => {
     event.preventDefault()
   }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post('/auth/login',
+        JSON.stringify({
+          email,
+          password: pwd
+        }), {
+          headers: { 'Content-Type': 'application/json' }
+        }
+      )
+
+      setAuth({ email })
+      setEmail('')
+      setPwd('')
+      setSuccess(true)
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg('No server response')
+      } else if (err.response?.status === 400) {
+        setErrMsg('Missing Username or Password')
+      } else if (err.response?.status === 401) {
+        setErrMsg('Unauthorized')
+      } else {
+        setErrMsg('Login failed')
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (success) {
+      navigate('/home')
+    }
+  }, [success])
 
   return (<>
     <Toolbar />
@@ -38,20 +89,26 @@ function LoginPage () {
         <Box
           component="form"
           autoComplete="off"
+          onSubmit={handleSubmit}
           sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
           <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
             <TextField
-              required
               id="outlined-required"
-              label="Correo electronico"
               type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              label="Correo electronico"
               placeholder="Correo electronico"
+              required
             />
             <FormControl variant="outlined">
               <InputLabel htmlFor="outlined-adornment-password">Contraseña</InputLabel>
               <OutlinedInput
                 id="outlined-adornment-password"
                 type={showPassword ? 'text' : 'password'}
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                label="Contraseña"
                 endAdornment={
                   <InputAdornment position="end">
                     <IconButton
@@ -64,10 +121,9 @@ function LoginPage () {
                     </IconButton>
                   </InputAdornment>
                 }
-                label="Contraseña"
               />
             </FormControl>
-            <Button type='submit' variant='contained' size='large'>Incia Sesión</Button>
+            <Button type='submit' variant='contained' size='large'>Inicia Sesión</Button>
           </Box>
         </Box>
         <Box sx={{
---
+++ web/src/routes/SignUp/SignUpPage.jsx
@@ -0,0 +1,195 @@
+import { Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, Toolbar, Typography, Box, TextField } from '@mui/material'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { EMAIL_REGEX, PWD_REGEX, USER_REGEX } from '../../utils/regex'
+import axios from '../../api/axios'
+
+function SignUpPage () {
+  const [user, setUser] = useState('')
+  const [validUser, setValidUser] = useState(false)
+
+  const [email, setEmail] = useState('')
+  const [validEmail, setValidEmail] = useState(false)
+
+  const [pwd, setPwd] = useState('')
+  const [validPwd, setValidPwd] = useState(false)
+
+  const [matchPwd, setMatchPwd] = useState('')
+  const [validMatch, setValidMatch] = useState(false)
+
+  const [errMsg, setErrMsg] = useState('')
+  const [success, setSuccess] = useState(false)
+
+  const [showPassword, setShowPassword] = useState(false)
+
+  useEffect(() => {
+    setValidEmail(EMAIL_REGEX.test(email))
+  }, [email])
+
+  useEffect(() => {
+    setValidUser(USER_REGEX.test(user))
+  })
+
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(pwd))
+    setValidMatch(pwd === matchPwd)
+  }, [pwd, matchPwd])
+
+  useEffect(() => {
+    setErrMsg('')
+  }, [email, pwd, matchPwd])
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show)
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault()
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const testUser = USER_REGEX.test(user)
+    const testEmail = EMAIL_REGEX.test(email)
+    const testPwd = PWD_REGEX.test(pwd)
+
+    if (!testUser || !testEmail || !testPwd) {
+      setErrMsg('Invalid Entry')
+    }
+    try {
+      const response = await axios.post('/auth/signup',
+        JSON.stringify({
+          username: user,
+          password: pwd,
+          email
+        }),
+        {
+          headers: { 'Content-Type': 'application/json' }
+        })
+      console.log(response.data)
+      setSuccess(!!response.data)
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg('No Server Response')
+      } else if (err.response?.status === 409) {
+        setErrMsg('Username or Email taken')
+      } else {
+        setErrMsg('Registration failed')
+      }
+    }
+  }
+
+  return (
+    <>
+      <Toolbar />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center',
+          py: '20px',
+          mx: { sm: '10px', md: 0 }
+        }}
+      >
+        <Box sx={{
+          background: '#ffffff',
+          display: 'flex',
+          justifyContent: ' center',
+          flexDirection: 'column',
+          width: { sm: '340px', md: '450px' },
+          gap: 3,
+          p: '30px',
+          borderRadius: '10px'
+        }}>
+          <Typography variant='h1' sx={{ fontSize: '1.4em', textAlign: 'center', mb: '10px' }}>Crear tu cuenta en AlingMind</Typography>
+          <Box
+            component="form"
+            autoComplete="off"
+            onSubmit={handleSubmit}
+            sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+              <TextField
+                type="text"
+                id="outlined-required"
+                error={!validUser && user !== ''}
+                label="Usuario"
+                placeholder="Usuario"
+                onChange={(e) => setUser(e.target.value)}
+                arial-invalid={validUser ? 'false' : 'true'}
+                required
+              />
+              <TextField
+                type="email"
+                id="outlined-required"
+                error={!validEmail && email !== ''}
+                label="Correo electronico"
+                placeholder="Correo electronico"
+                onChange={(e) => setEmail(e.target.value)}
+                arial-invalid={validUser ? 'false' : 'true'}
+                required
+              />
+              <FormControl variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Contraseña</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={showPassword ? 'text' : 'password'}
+                  error={!validPwd && pwd !== ''}
+                  onChange={(e) => setPwd(e.target.value)}
+                  placeholder="Password"
+                  label="Contraseña"
+                  required
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+              <FormControl variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Repite la contraseña</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={showPassword ? 'text' : 'password'}
+                  error={!validMatch && matchPwd !== ''}
+                  disabled={!validPwd}
+                  onChange={(e) => setMatchPwd(e.target.value)}
+                  label="Repite la contraseña"
+                  required
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+              <Button disabled={!!(!validPwd || !validEmail || !validMatch || !validUser)} type='submit' variant='contained' size='large'>Crear cuenta</Button>
+              <Typography paragraph>{errMsg}</Typography>
+            </Box>
+          </Box>
+          <Box sx={{
+            display: 'flex', flexDirection: 'column', gap: 2
+          }}>
+            <Typography variant='h2' sx={{ fontSize: '18px', textAlign: 'center' }}>¿Ya tienes cuenta?</Typography>
+            <Button component={Link} variant='outlined' size='large' to='/login'>Iniciar sesión</Button>
+          </Box>
+        </Box>
+      </Box >
+    </>
+  )
+}
+
+export { SignUpPage }
