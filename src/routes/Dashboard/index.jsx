86440340c6577baec6bfe995d113795b73fea6fe
Sebastian Beltran <bjohansebas@gmail.com>
2023-01-19 17:41:51 -0500

    Add statistics in dashboard

--- src/routes/Dashboard/index.jsx
+++ src/routes/Dashboard/index.jsx
@@ -1,19 +1,70 @@
-import { Box, List, IconButton, Menu, MenuItem, Typography, ListItem, ListItemButton, ListItemText } from '@mui/material'
+import 'chart.js/auto'
+import { Box, Button, Divider, List, IconButton, Menu, MenuItem, Typography, ListItem, ListItemButton, ListItemText } from '@mui/material'
 import { useEffect, useState } from 'react'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
+import CircleIcon from '@mui/icons-material/Circle'
+import FilterListIcon from '@mui/icons-material/FilterList'
 import axios from '../../api/axios'
 import { useAuth } from '../../hooks/useAuth'
 import { useNavigate } from 'react-router-dom'
+import { Doughnut } from 'react-chartjs-2'
 
 function DashboardPage () {
   const navigate = useNavigate()
   const { userInfo, userId, credentials } = useAuth()
   const [anchorEl, setAnchorEl] = useState(null)
+  const [anchorElStatistics, setAnchorElStatistics] = useState(null)
   const [allPlaces, setAllPlaces] = useState([])
   const [idSelect, setIdSelect] = useState('')
+  const [filter, setFilter] = useState('all')
+  const [emotions, setEmotions] = useState([0, 0])
+  const [labels, setLabels] = useState(['Positivo', 'Negativo'])
+  const [dataEmotions, setDataEmotions] = useState([0, 0])
+  const [colors, setColors] = useState(['red', 'blue'])
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Emociones',
+        data: dataEmotions,
+        backgroundColor: colors,
+        borderWidth: 1,
+        borderColor: '#00575C'
+      }
+    ]
+  }
+
+  useEffect(() => {
+    if (filter === 'positive') {
+      filterPositive()
+    } else if (filter === 'negative') {
+      filterNegative()
+    } else {
+      filterAll()
+    }
+  }, [filter])
 
   useEffect(() => {
     getPlace()
+  }, [])
+
+  useEffect(() => {
+    async function getDataEmotions () {
+      try {
+        const response = await axios.get(`/statistics/${userId}/`, {
+          headers: {
+            Authorization: `Bearer ${credentials}`
+          }
+        })
+
+        setEmotions([...response?.data.data])
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
+    getDataEmotions()
   }, [])
 
   const getPlace = async () => {
@@ -66,6 +117,14 @@ function DashboardPage () {
     }
   }
 
+  const handleStatisticsMenu = (event) => {
+    if (anchorElStatistics) {
+      setAnchorElStatistics(null)
+    } else {
+      setAnchorElStatistics(event.currentTarget)
+    }
+  }
+
   const onDelete = async () => {
     try {
       setAnchorEl(null)
@@ -79,12 +138,125 @@ function DashboardPage () {
       console.log(err)
     }
   }
+
+  const filterAll = async () => {
+    const label = []
+    const dataResult = []
+    const color = []
+
+    try {
+      const response = await axios.get(`/statistics/${userId}/all`, {
+        headers: {
+          Authorization: `Bearer ${credentials}`
+        }
+      })
+
+      const dataResponse = response?.data.data
+      const positive = dataResponse.positive
+      const negative = dataResponse.negative
+      for await (const value of positive) {
+        label.push(value[0])
+        dataResult.push(value[1])
+        color.push(`#${value[2]}`)
+      }
+      for await (const value of negative) {
+        label.push(value[0])
+        dataResult.push(value[1])
+        color.push(`#${value[2]}`)
+      }
+
+      setLabels(label)
+      setDataEmotions(dataResult)
+      setColors(color)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const filterPositive = async () => {
+    const label = []
+    const dataResult = []
+    const color = []
+
+    try {
+      const response = await axios.get(`/statistics/${userId}/positive`, {
+        headers: {
+          Authorization: `Bearer ${credentials}`
+        }
+      })
+
+      const dataResponse = response?.data.data
+      for await (const value of dataResponse) {
+        label.push(value[0])
+        dataResult.push(value[1])
+        color.push(`#${value[2]}`)
+      }
+
+      setLabels(label)
+      setDataEmotions(dataResult)
+      setColors(color)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const filterNegative = async () => {
+    const label = []
+    const dataResult = []
+    const color = []
+
+    try {
+      const response = await axios.get(`/statistics/${userId}/negative`, {
+        headers: {
+          Authorization: `Bearer ${credentials}`
+        }
+      })
+
+      const dataResponse = response?.data.data
+      for await (const value of dataResponse) {
+        label.push(value[0])
+        dataResult.push(value[1])
+        color.push(`#${value[2]}`)
+      }
+
+      setLabels(label)
+      setDataEmotions(dataResult)
+      setColors(color)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   return (
     <Box
       component="main"
-      sx={{ width: '100%', px: { xs: '20px', sm: '50px', md: '100px' }, py: '20px' }}>
-      <Box component="header" sx={{ my: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-        <Typography variant="h1" fontWeight="700" sx={{ fontSize: '1.4em', textAlign: 'center' }}>Bienvenido, {userInfo?.firstName} {userInfo?.lastName}</Typography>
+      sx={{ width: '100%', px: { xs: '10px', sm: '50px', md: '100px' }, py: '20px' }}>
+
+      <Box component="header" sx={{ my: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: 3 }}>
+        <Typography variant="h1" fontWeight="700" sx={{ fontSize: '1.7em', textAlign: 'center' }}>Bienvenido, {userInfo?.firstName} {userInfo?.lastName}</Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Box sx={{
+            display: 'flex',
+            background: 'rgba(0, 87, 92, 0.04)',
+            alignItems: 'center',
+            borderRadius: { xs: '30px', sm: '60px' },
+            flexDirection: { xs: 'column', sm: 'row' },
+            height: { xs: 'auto', md: '48px' },
+            py: '8px',
+            px: '16px',
+            gap: {
+              xs: 1, sm: 3
+            }
+          }}>
+            <Typography>Tus emociones</Typography>
+            <Divider orientation='vertical' sx={{ display: { xs: 'none', sm: 'block' } }} />
+            <Divider orientation='horizontal' sx={{ display: { xs: 'block', sm: 'none' }, width: '100%' }} />
+            <Box sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' } }}>
+              <Button startIcon={<CircleIcon sx={{ color: '#01FF45' }} />}>{emotions[0]} Emociones positivas</Button>
+              <Button startIcon={<CircleIcon sx={{ color: '#FE1808' }} />}>{emotions[1]} Emociones negativas</Button>
+            </Box>
+          </Box>
+        </Box>
       </Box>
       <Box display="flex" flexDirection="column" gap={3}>
         <Typography variant="h2" fontWeight="600" sx={{ fontSize: '1.4em' }}>Tus lugares</Typography>
@@ -129,6 +301,58 @@ function DashboardPage () {
           <MenuItem key="2" onClick={() => navigate(`/place/${idSelect}/edit`)}>Editar lugar</MenuItem>
         </Menu>
       </Box>
+      <Box sx={{ my: '30px', background: '#ffffff', py: '20px', px: { xs: '0', sm: '20px' }, borderRadius: '10px' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', px: '10px' }}>
+          <Typography variant="h6">Tus estadisticas</Typography>
+          <Button variant="text" startIcon={<FilterListIcon />} onClick={handleStatisticsMenu}>Filtrar</Button>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Box sx={{ height: '100%' }}>
+            <Doughnut data={data} options={{
+              responsive: true,
+              maintainAspectRatio: false,
+              cutout: 0,
+              layout: {
+                padding: {
+                  left: 0,
+                  top: 0,
+                  bottom: 0,
+                  right: 0
+                }
+              }
+            }} width={300} height={300} />
+          </Box>
+        </Box>
+      </Box >
+      <Menu
+        sx={{ mt: '40px', zIndex: 1202 }}
+        id="menu-appbar"
+        anchorEl={anchorElStatistics}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right'
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right'
+        }}
+        open={Boolean(anchorElStatistics)}
+        onClose={handleStatisticsMenu}
+      >
+        <MenuItem key="1" onClick={() => {
+          setFilter('all')
+          setAnchorElStatistics(null)
+        }}>Todos tus emociones</MenuItem>
+        <MenuItem key="2" onClick={() => {
+          setFilter('negative')
+          setAnchorElStatistics(null)
+        }}>Solo emociones negativas</MenuItem>
+        <MenuItem key="3" onClick={() => {
+          setFilter('positive')
+          setAnchorElStatistics(null)
+        }}>Solo emociones positivas</MenuItem>
+      </Menu>
     </Box >)
 }
 
