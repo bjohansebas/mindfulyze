7433bc901a0465168d8cc6072fb193f18a055f06
Sebastian Beltran <bjohansebas@gmail.com>
2023-02-26 21:35:42 -0500

    feat(Root): Add versioning to routes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
