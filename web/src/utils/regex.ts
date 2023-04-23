408ad14ab443e459918c36c566e4356c1df627ae
Sebastian Beltran <bjohansebas@gmail.com>
2023-04-23 17:02:48 -0500

    fix(web): Change regex for user and gender because not validate well and Add regex with typescript

---
+++ web/src/utils/regex.ts
@@ -0,0 +1,6 @@
+export const NAMES_REGEX: RegExp = /^[A-ZÑa-zñáéíóúÁÉÍÓÚ'°][A-ZÑa-zñáéíóúÁÉÍÓÚ'°]{2,22}(?: [A-ZÑa-zñáéíóúÁÉÍÓÚ'°][A-ZÑa-zñáéíóúÁÉÍÓÚ'°]{2,22})?$/
+export const GENDER_REGEX: RegExp = /^(male|female|other)$/i
+export const DATE_REGEX: RegExp = /^([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/
+export const EMAIL_REGEX: RegExp = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
+export const PWD_REGEX: RegExp = /^(?=.*[a-z])(?=.*[0-9]).{8,30}$/
+export const HEXADECIMAL_REGEX: RegExp = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
