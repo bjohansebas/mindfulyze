abfb0834d4bbc2a2b483cb12c0c6bccefc123efc
Sebastian Beltran <bjohansebas@gmail.com>
2023-04-30 22:47:34 -0500

    feat(web): Add shortcuts paths alias and add config of svg plugin

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -1,1 +1,2 @@
 import 'vite/client'
+import 'vite-plugin-svgr/client'
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import path from "path";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "components": path.resolve(__dirname, "src", "components"),
+      "hooks": path.resolve(__dirname, "src", "hooks"),
+      "routes": path.resolve(__dirname, "src", "routes"),
+      "services": path.resolve(__dirname, "src", "services"),
+      "assets": path.resolve(__dirname, "src", "assets"),
+      "context": path.resolve(__dirname, "src", "context"),
+      "translations": path.resolve(__dirname, "src", "translations"),
+      "utils": path.resolve(__dirname, "src", "utils"),
+      "api": path.resolve(__dirname, "src", "api")
+    }
+  },
+  plugins: [react(), svgr()]
 })
