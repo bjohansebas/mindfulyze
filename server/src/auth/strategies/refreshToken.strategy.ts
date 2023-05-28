12e31c919b7c09590aab2e3f63bd9d26310e4135
Sebastian Beltran <bjohansebas@gmail.com>
2023-05-28 12:50:38 -0500

    feat(server): Add verify refresh token on refresh strategy

--- server/src/auth/strategies/refreshToken.strategy.ts
+++ server/src/auth/strategies/refreshToken.strategy.ts
@@ -1,9 +1,16 @@
 import { PassportStrategy } from '@nestjs/passport';
+import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Request } from 'express';
-import { Inject, Injectable } from '@nestjs/common';
+
+import * as argon2 from 'argon2';
+
 import config from '../../config';
-import { ConfigType } from '@nestjs/config';
+
+import { User } from '../../modules/users/entities/user.entity';
+
+import { UsersService } from '../../modules/users/services/users.service';
 
 @Injectable()
 export class RefreshTokenStrategy extends PassportStrategy(
@@ -12,6 +19,7 @@ export class RefreshTokenStrategy extends PassportStrategy(
 ) {
   constructor(
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
+    private userService: UsersService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -21,8 +29,29 @@ export class RefreshTokenStrategy extends PassportStrategy(
     });
   }
 
-  validate(req: Request, payload: any) {
+  async validate(req: Request, payload: any) {
     const refreshToken = req.get('Authorization').replace('Bearer', '').trim();
+
+    const existUser: User = await this.userService.findAccount(payload['sub']);
+    const encryptTokens: string[] = existUser.refreshTokens;
+
+    let refreshTokenMatch = false;
+
+    await Promise.all(
+      encryptTokens.map(async (encryptToken) => {
+        const isMatch: boolean = await argon2.verify(
+          encryptToken,
+          refreshToken,
+        );
+
+        if (isMatch) {
+          refreshTokenMatch = true;
+        }
+      }),
+    );
+
+    if (!refreshTokenMatch) throw new ForbiddenException('Access Denied');
+
     return { ...payload, refreshToken };
   }
 }
