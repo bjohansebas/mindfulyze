0d0fc69839f0d200319565c30b9faa51436b36a4
Sebastian Beltran <bjohansebas@gmail.com>
2023-06-22 21:55:53 -0500

    feat(web): Add vercel web analytics

--- web/src/routes/Main/index.tsx
+++ web/src/routes/Main/index.tsx
@@ -1,10 +1,12 @@
 import { AppProvider } from '@/context/appContext'
 import { Outlet } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 
 function MainPage (): JSX.Element {
   return (
     <AppProvider>
       <Outlet />
+      <Analytics />
     </AppProvider>
   )
 }
