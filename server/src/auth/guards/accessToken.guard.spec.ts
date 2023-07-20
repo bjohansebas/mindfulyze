02f7d9439dc9a5f45fdf6f12dbcd807c57e3f115
Sebastian Beltran <bjohansebas@gmail.com>
2023-07-20 10:00:49 -0500

    test(server): Remove tests

--- server/src/auth/guards/accessToken.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AccessTokenGuard } from './accessToken.guard';
-
-describe('JwtGuard', () => {
-  it('should be defined', () => {
-    expect(new AccessTokenGuard()).toBeDefined();
-  });
-});
