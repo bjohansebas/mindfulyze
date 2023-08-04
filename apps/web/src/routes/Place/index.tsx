5db50cbc26a3381a6ac0f9e03fc2612552ae2ba6
Sebastian Beltran <bjohansebas@gmail.com>
2023-08-03 19:52:48 -0500

    feat(apps/web): Delete place pages

--- apps/web/src/routes/Place/index.tsx
+++
@@ -1,5 +0,0 @@
-import { EditPlacePage } from './EditPlace'
-import { NewPlacePage } from './NewPlace'
-import { ShowPlacePage } from './ShowPlace'
-
-export { EditPlacePage, NewPlacePage, ShowPlacePage }
