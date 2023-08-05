152485c0bd1d7958da7135333a528f479be64dce
Sebastian Beltran <bjohansebas@gmail.com>
2023-08-04 22:08:55 -0500

    feat(apps/web): Add new ui for signup

--- apps/web/src/app/signup/layout.tsx
+++ apps/web/src/app/signup/layout.tsx
@@ -1,8 +1,9 @@
 import { useApp } from '@/hooks/useApp'
 import { useEffect } from 'react'
+import { Helmet } from 'react-helmet-async'
 import { Outlet, useNavigate } from 'react-router-dom'
 
-export function SignLayoutPage(): JSX.Element {
+export const SignLayoutPage = (): JSX.Element => {
   const navigate = useNavigate()
   const { credential, userId } = useApp()
 
@@ -12,5 +13,14 @@ export function SignLayoutPage(): JSX.Element {
     }
   }, [])
 
-  return <Outlet />
+  return (
+    <>
+      <Helmet>
+        <title>Sign up | Mindfulyze</title>
+      </Helmet>
+      <Outlet />
+    </>
+  )
 }
+
+export default SignLayoutPage
--- apps/web/src/app/signup/page.tsx
+++ apps/web/src/app/signup/page.tsx
@@ -1,6 +1,3 @@
-import { Box, Button } from '@mui/material'
-import { Helmet } from 'react-helmet-async'
-
 import { useState, type FormEvent } from 'react'
 import { FormattedMessage } from 'react-intl'
 import { Link, useNavigate } from 'react-router-dom'
@@ -11,27 +8,25 @@ import { EMAIL_REGEX, PWD_REGEX } from 'utils/regex'
 
 import { Banner } from '@/components/Banner'
 import { EmailField } from '@/components/Fields/Email'
+import { Button } from '@nextui-org/react'
 import { PasswordTextField } from 'components/Fields/Password'
 
 export const SignUpPage = (): JSX.Element => {
   const { loginAction } = useApp()
   const navigate = useNavigate()
 
-  const [email, setEmail] = useState<string>('')
-  const [validEmail, setValidEmail] = useState<boolean>(false)
+  const [email, setEmail] = useState<string | undefined>('')
 
   const [pwd, setPwd] = useState<string>('')
-  const [validPwd, setValidPwd] = useState<boolean>(false)
 
   const [confirmPwd, setConfirmPwd] = useState<string>('')
-  const [validConfirmPwd, setValidConfirmPwd] = useState<boolean>(false)
 
   const [loading, setLoading] = useState(false)
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault()
     setLoading(true)
-
+    if (email == null) return
     const testEmail = EMAIL_REGEX.test(email)
     const testPwd = PWD_REGEX.test(pwd)
 
@@ -54,104 +49,56 @@ export const SignUpPage = (): JSX.Element => {
   }
 
   return (
-    <>
-      <Helmet>
-        <title>Sign up | Mindfulyze</title>
-      </Helmet>
-      <Box
-        sx={{
-          minHeight: '100vh',
-          width: '100vw',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'column',
-          background: 'linear-gradient(45deg, #00575C 0%, #002d32 100%)',
-          backgroundPosition: '0 0',
-          backgroundSize: '100% 100%',
-          py: { sm: '40px', xs: '0' },
-        }}
-      >
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: ' center',
-            flexDirection: 'column',
-            width: { xs: '90%', sm: '60%', md: '50%', lg: '40%' },
-            gap: 4,
-            p: { sm: '40px 32px', xs: '40px 16px' },
-            borderRadius: '12px',
-          }}
-        >
-          <header className='flex items-center gap-6 flex-col'>
+    <div className='min-h-screen w-screen flex justify-center items-center flex-col sm:py-10'>
+      <div className='shadow-xl border flex justify-center flex-col gap-8 rounded-xl max-w-[90%] sm:max-w-[60%] md:max-w-[50%] lg:max-w-[40%] py-10 px-8 max-[640px]:px-4'>
+        <header className='flex items-center gap-6 flex-col text-primary-800'>
+          <Link to='/'>
             <Banner widthFavicon={40} heightText={24.26} widthText={230} />
-            <div className='w-full px-2 gap-2 flex flex-col'>
-              <h1 className='text-3xl font-extrabold'>Sign Up</h1>
-              <h2 className='text-xl font-semibold'>Sign up now and begin your journey of self-discovery!</h2>
-            </div>
-          </header>
-          <Box
-            component='form'
-            autoComplete='off'
-            onSubmit={async (e) => {
-              await handleSubmit(e)
-            }}
-            sx={{ display: 'flex', flexDirection: 'column', gap: '32px' }}
-          >
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-              <EmailField
-                text={email}
-                setText={setEmail}
-                setValid={setValidEmail}
-                errorRequest=''
-                requiredValid={true}
-                errorText='Enter a valid email address'
-                label={<FormattedMessage id='login.email' defaultMessage='Email' />}
-              />
-              <PasswordTextField
-                text={pwd}
-                setText={setPwd}
-                errorRequest=''
-                requiredValid={true}
-                setValid={setValidPwd}
-                errorText='The password must have at least one lowercase letter, one number, and a length between 8 and 30 characters'
-                label={<FormattedMessage id='signup.password' defaultMessage='Password' />}
-              />
-              <PasswordTextField
-                text={confirmPwd}
-                setText={setConfirmPwd}
-                errorRequest=''
-                errorText='The passwords do not match. Please make sure both passwords are the same.'
-                comparePassword={pwd}
-                isDisable={true}
-                setValid={setValidConfirmPwd}
-                label={<FormattedMessage id='signup.confirm' defaultMessage='Confirm password' />}
-              />
-            </Box>
-            <Box display='flex' sx={{ gap: { sm: '32px', xs: '16px' }, flexDirection: { xs: 'column', sm: 'row' } }}>
-              <Button
-                disabled={!validPwd || !validConfirmPwd || !validEmail || loading}
-                type='submit'
-                variant='contained'
-                size='large'
-                sx={{ width: '100%', borderRadius: '12px' }}
-              >
-                <FormattedMessage id='signup.submit' defaultMessage='Sign up' />
-              </Button>
-              <Button
-                component={Link}
-                variant='outlined'
-                size='large'
-                to='/login'
-                sx={{ width: { sm: '160px', sx: '100%' }, borderRadius: '12px' }}
-              >
+          </Link>
+          <div className='w-full px-2 gap-2 flex flex-col'>
+            <h1 className='text-3xl font-extrabold'>Sign Up</h1>
+            <h2 className='text-xl font-semibold'>Sign up now and begin your journey of self-discovery!</h2>
+          </div>
+        </header>
+        <form autoComplete='off' onSubmit={handleSubmit} className='flex flex-col gap-8'>
+          <div className='flex flex-col gap-4'>
+            <EmailField
+              text={email}
+              setText={setEmail}
+              requiredValid={true}
+              errorText='Enter a valid email address'
+              label={<FormattedMessage id='login.email' defaultMessage='Email' />}
+            />
+            <PasswordTextField
+              text={pwd}
+              setText={setPwd}
+              requiredValid
+              errorText='The password must have at least one lowercase letter, one number, and a length between 8 and 30 characters'
+              label={<FormattedMessage id='signup.password' defaultMessage='Password' />}
+            />
+            <PasswordTextField
+              text={confirmPwd}
+              setText={setConfirmPwd}
+              errorText='The passwords do not match. Please make sure both passwords are the same.'
+              comparePassword={pwd}
+              isDisable
+              requiredValid
+              label={<FormattedMessage id='signup.confirm' defaultMessage='Confirm password' />}
+            />
+          </div>
+          <div className='flex gap-4 max-[640px]:gap-4 flex-row max-[640px]:flex-col'>
+            <Button type='submit' color='primary' variant='solid' size='lg' disabled={loading} className='w-full'>
+              <FormattedMessage id='signup.submit' defaultMessage='Sign up' />
+            </Button>
+            <Link to='/login'>
+              <Button size='lg' color='primary' type='button'>
                 <FormattedMessage id='signup.login.link' defaultMessage='Log in' />
               </Button>
-            </Box>
-          </Box>
-        </Box>
-      </Box>
-    </>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
