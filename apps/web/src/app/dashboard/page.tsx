e048f0f795b439d41d74f1e61747bba0e7ef4ff1
Sebastian Beltran <bjohansebas@gmail.com>
2023-08-04 22:08:11 -0500

    feat(apps/web): Delete components of dashboard

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,18 +1,11 @@
-import { Box, Stack } from '@mui/material'
 import { Helmet } from 'react-helmet-async'
-import { UserWelcomeHeader } from './Header'
-import { ShowStatistics } from './Statistics/ShowStatistics'
 
 export function DashboardPage(): JSX.Element {
   return (
-    <Box component='main' sx={{ width: '100%', px: { xs: '10px', sm: '50px', md: '100px' }, py: '20px' }}>
+    <div className='w-full'>
       <Helmet>
         <title>Dashboard | Mindfulyze</title>
       </Helmet>
-      <Stack spacing={2} px='4px'>
-        <UserWelcomeHeader />
-        <ShowStatistics />
-      </Stack>
-    </Box>
+    </div>
   )
 }
