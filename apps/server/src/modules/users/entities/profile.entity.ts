d1ba18a46a4af188a84b06a74deee7d34d452076
Sebastian Beltran <bjohansebas@gmail.com>
2023-08-12 13:15:06 -0500

    fix(apps/server): Fix foreign Key Constraint Name of relation between profile and user

--- apps/server/src/modules/users/entities/profile.entity.ts
+++ apps/server/src/modules/users/entities/profile.entity.ts
@@ -27,7 +27,7 @@ export class ProfileUser {
     onDelete: 'CASCADE',
     onUpdate: 'CASCADE',
   })
-  @JoinColumn({ name: 'user_id', foreignKeyConstraintName: 'fk_user_profile' })
+  @JoinColumn({ name: 'user_id', foreignKeyConstraintName: 'fk_profile_user' })
   user: Relation<User>
   @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
   createdAt: Date
