70ffa74f5741db049032859755a5f01178b54506
Sebastian Beltran <bjohansebas@gmail.com>
2023-08-16 19:09:46 -0500

    feat(apps/web): Add capitalize util

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function capitalize(str: string) {
+  if (!str || typeof str !== 'string') return str
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
