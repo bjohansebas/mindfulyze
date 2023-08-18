fd6cbc39f82c123ddcd8ba6f33a63f5688bf489b
Sebastian Beltran <bjohansebas@gmail.com>
2023-08-17 21:10:15 -0500

    feat(Root): Add get thoughts by id and user

--- app/api/thoughts/route.ts
+++ app/api/thoughts/route.ts
@@ -1,15 +1,41 @@
 import { authOptions } from '@/lib/auth'
 import prisma from '@/lib/prisma'
 
-import { getServerSession } from 'next-auth'
-import { NextResponse } from 'next/server'
 import { validateThought } from 'schemas/thought'
 
+import { getServerSession } from 'next-auth'
+import { NextResponse } from 'next/server'
+
+// GET /api/thoughts
+// Get thoughts of user
+export async function GET() {
+  const session = await getServerSession(authOptions)
+
+  if (!session || !session.user.id) {
+    return NextResponse.json({ message: 'You must be logged in.' }, { status: 401 })
+  }
+
+  try {
+    const response = await prisma.thought.findMany({
+      where: {
+        userId: session.user.id,
+      },
+    })
+
+    return NextResponse.json(response)
+  } catch (e) {
+    if (e.name === 'NotFoundError') {
+      return NextResponse.json({ error: e.name, message: 'Not found thought' }, { status: 404 })
+    }
+  }
+}
+
 // POST /api/thoughts
+// Create new thought for user
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions)
 
-  if (!session || !session.user?.email) {
+  if (!session) {
     return NextResponse.json({ message: 'You must be logged in.' }, { status: 401 })
   }
 
@@ -19,16 +45,16 @@ export async function POST(req: Request) {
     return NextResponse.json({ error: JSON.parse(result.error.message) }, { status: 422 })
   }
 
-  const response = await prisma.thought.create({
-    data: {
-      text: result.data.text,
-      user: {
-        connect: {
-          email: session?.user.email,
-        },
+  try {
+    const response = await prisma.thought.create({
+      data: {
+        text: result.data.text,
+        userId: session.user.id,
       },
-    },
-  })
+    })
 
-  return NextResponse.json({ response })
+    return NextResponse.json(response)
+  } catch (e) {
+    return NextResponse.json(e, { status: 400 })
+  }
 }
