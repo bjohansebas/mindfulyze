b172431cb24b81a53ce5db2bc9cc3ff881679d58
Sebastian Beltran <bjohansebas@gmail.com>
2023-08-25 17:34:15 -0500

    feat(Root): Add loading editor thought

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,9 +1,24 @@
 import { EditorThought } from '@/ui/dashboard/editorThought'
+import { Skeleton } from '@/ui/skeleton'
+
+import { Suspense } from 'react'
 
 export default function Page() {
   return (
     <div className='flex flex-col items-center w-full mt-6'>
-      <EditorThought />
+      <Suspense
+        fallback={
+          <div className='flex flex-col  gap-1 sm:w-2/4 min-w-[300px] w-screen rounded-lg'>
+            <Skeleton className=' min-h-48 h-48 w-full min-w-[300px] max-h-56 p-6' />
+            <div className='flex justify-between items-center px-6 py-2'>
+              <Skeleton className='w-[240px] h-9' />
+              <Skeleton className='h-9 w-[75.95px]' />
+            </div>
+          </div>
+        }
+      >
+        <EditorThought />
+      </Suspense>
     </div>
   )
 }
