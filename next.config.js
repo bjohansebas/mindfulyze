1b0b5b2a1f7617f491c23da2404f8930e6cf8ad4
Sebastian Beltran <bjohansebas@gmail.com>
2023-08-25 21:02:30 -0500

    chore(Root): Add new domains for images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ['lh3.googleusercontent.com', 'res.cloudinary.com'],
+  },
 }
 
+// module.exports = nextConfig
 module.exports = withContentlayer(nextConfig)
