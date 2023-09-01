d4168cb0a1591dee07fe4e7875d6b7a4167165a2
Sebastian Beltran <bjohansebas@gmail.com>
2023-08-31 20:39:35 -0500

    refactor(Root): Refactor metadata

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -1,10 +1,10 @@
 import { Metadata } from 'next'
-import { HOME_DOMAIN } from './constants'
+import { HOME_DOMAIN, MINDFULYZE_THUMBNAIL } from './constants'
 
 export function constructMetadata({
   title = 'Mindfulyze - Record moments from your day, free your thoughts and emotions in a safe way.',
   description = 'Capture the moments that shape your day. Express yourself freely, alleviate your thoughts, and release your emotions in a secure and private environment. Our platform offers a safe space for you to document your day, helping you gain perspectives, preserve memories, and foster personal growth. Start your journey of self-expression and reflection today.                           ',
-  image = 'https://res.cloudinary.com/mindfulyze/image/upload/v1693177063/thumbnail.png',
+  image = MINDFULYZE_THUMBNAIL,
   icons = '/favicon.ico',
 }: {
   title?: string
