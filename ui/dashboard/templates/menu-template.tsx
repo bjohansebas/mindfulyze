f29f56d2828e999e71c52044ad258ee7ca516707
Sebastian Beltran <bjohansebas@gmail.com>
2023-09-09 18:00:56 -0500

    fix(Root): Fix typo in menu template

--- ui/dashboard/templates/menu-template.tsx
+++ ui/dashboard/templates/menu-template.tsx
@@ -1,7 +1,15 @@
 import { Button } from '@/ui/button'
 import { Dispatch, SetStateAction } from 'react'
 import { ChevronDownIcon, PlusIcon } from '@heroicons/react/24/solid'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/ui/dropdown-menu'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/ui/dropdown-menu'
 import { TemplateResponse } from '@/app/actions/templates'
 
 interface DialogTemplateProps {
@@ -17,32 +25,30 @@ export async function MenuTemaplate({ setIsOpen, templates }: DialogTemplateProp
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <Button className='p-1 rounded-l-none'>
+        <Button className="p-1 rounded-l-none">
           <ChevronDownIcon className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-[360px]">
         <DropdownMenuLabel>Templates for thoughts</DropdownMenuLabel>
         <DropdownMenuGroup>
-          {templatesData.data.length > 0 ?
-            templatesData.data.map(({ title, default }) =>
-              <DropdownMenuItem className='flex justify-between'>
-                <span>Title</span>
+          {templatesData.data.length > 0 ? (
+            templatesData.data.map((data) => (
+              <DropdownMenuItem className="flex justify-between">
+                <span>{data.title}</span>
               </DropdownMenuItem>
-            )
-            : <div className='p-5 flex items-center flex-col gap-5'>
-              <p className='text-center'>
-                Oops, it looks like you don't have any templates.
-              </p>
-              <DropdownMenuItem onClick={handleOpenTemplate} className='text-primary-700'>
+            ))
+          ) : (
+            <div className="p-5 flex items-center flex-col gap-5">
+              <p className="text-center">Oops, it looks like you don't have any templates.</p>
+              <DropdownMenuItem onClick={handleOpenTemplate} className="text-primary-700">
                 Create your first template.
               </DropdownMenuItem>
             </div>
-          }
-
+          )}
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={handleOpenTemplate} className='text-primary-700'>
+        <DropdownMenuItem onClick={handleOpenTemplate} className="text-primary-700">
           <PlusIcon className="mr-2 h-4 w-4" />
           New template
         </DropdownMenuItem>
