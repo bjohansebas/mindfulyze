f1cf5040cf8dea73cd83af4f65e8f97c665c31eb
Sebastian Beltran <bjohansebas@gmail.com>
2023-09-12 18:46:26 -0500

    refactor(Root): Fix code style

--- ui/editor/props.ts
+++ ui/editor/props.ts
@@ -8,7 +8,7 @@ export const TiptapEditorProps: EditorProps = {
   handleDOMEvents: {
     keydown: (_view, event) => {
       // prevent default event listeners from firing when slash command is active
-      if (["ArrowUp", "ArrowDown", "Enter"].includes(event.key)) {
+      if (['ArrowUp', 'ArrowDown', 'Enter'].includes(event.key)) {
         const slashCommand = document.querySelector('#slash-command')
         if (slashCommand) {
           return true
