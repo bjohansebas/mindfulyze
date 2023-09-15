b0bdfdacd5fa4a547d1863a36d8f0674f0ce8514
Sebastian Beltran <bjohansebas@gmail.com>
2023-09-15 00:54:19 +0000

    Fix: Fix thoughts placeholder for loading data

--- ui/dashboard/editor/placeholder-thoughts.tsx
+++ ui/dashboard/editor/placeholder-thoughts.tsx
@@ -1,13 +1,15 @@
 import { Skeleton } from '@/ui/skeleton'
 
 export default function ThoughtsPlaceholder() {
-  return (
-    <div className="flex flex-col  gap-1 sm:w-2/4 min-w-[300px] w-screen rounded-lg">
-      <Skeleton className=" min-h-48 h-48 w-full min-w-[300px] max-h-56 p-6" />
-      <div className="flex justify-between items-center px-6 py-2">
-        <Skeleton className="w-[240px] h-9" />
-        <Skeleton className="h-9 w-[75.95px]" />
+  return (<>
+    {Array.from({ length: 2 }).map((_, i) => (
+      <div className="flex flex-col  gap-1 sm:w-2/4 min-w-[300px] w-screen rounded-lg" key={i}>
+        <Skeleton className=" min-h-48 h-48 w-full min-w-[300px] max-h-56 p-6" />
+        <div className="flex justify-start items-center px-6 py-2">
+          <Skeleton className="w-[240px] h-9" />
+        </div>
       </div>
-    </div>
+    ))}
+  </>
   )
 }
