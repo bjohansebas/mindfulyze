2803ae185903f7dc00f7e93e4a805bf329fdeada
Sebastian Beltran <bjohansebas@gmail.com>
2023-09-22 01:30:45 +0000

    Chore: Update dependencies and Remove  rehype-pretty-code

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -214,25 +214,6 @@ export default makeSource({
     rehypePlugins: [
       rehypeSlug,
       [
-        rehypePrettyCode,
-        {
-          theme: 'one-dark-pro',
-          onVisitLine(node) {
-            // Prevent lines from collapsing in `display: grid` mode, and allow empty
-            // lines to be copy/pasted
-            if (node.children.length === 0) {
-              node.children = [{ type: 'text', value: ' ' }]
-            }
-          },
-          onVisitHighlightedLine(node) {
-            node.properties.className.push('line--highlighted')
-          },
-          onVisitHighlightedWord(node) {
-            node.properties.className = ['word--highlighted']
-          },
-        },
-      ],
-      [
         rehypeAutolinkHeadings,
         {
           properties: {
