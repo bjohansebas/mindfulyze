04f60041e8b2a0d514983e5365cacb4360f77fe8
Sebastian Beltran <bjohansebas@gmail.com>
2023-09-21 20:35:42 -0500

    fix(Root): Remove import rehype-pretty-code

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,7 +1,6 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 import GithubSlugger from 'github-slugger'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypePrettyCode from 'rehype-pretty-code'
 import rehypeSlug from 'rehype-slug'
 import remarkGfm from 'remark-gfm'
 
