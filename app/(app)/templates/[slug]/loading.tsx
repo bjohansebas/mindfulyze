2044c413315026bcc8d49470e75ec6e6a33da519
Sebastian Beltran <bjohansebas@gmail.com>
2023-09-30 16:31:41 -0500

    fix(templates): fix placeholder of loading template

--- app/(app)/templates/[slug]/loading.tsx
+++ app/(app)/templates/[slug]/loading.tsx
@@ -1,5 +1,11 @@
 import EditorTemplatesPlaceholder from '@/ui/dashboard/templates/placeholder-editor-templates'
 
 export default function Loading() {
-  return <EditorTemplatesPlaceholder />
+  return (
+    <div className="w-full flex justify-center p-6 h-screen">
+      <div className="border w-full flex flex-col rounded-2xl bg-card max-h-full overflow-y-auto">
+        <EditorTemplatesPlaceholder />
+      </div>
+    </div>
+  )
 }
--- ui/dashboard/templates/placeholder-editor-templates.tsx
+++ ui/dashboard/templates/placeholder-editor-templates.tsx
@@ -3,13 +3,13 @@ import { Skeleton } from '@/ui/skeleton'
 export default function EditorTemplatesPlaceholder() {
   return (
     <div className="border w-full flex flex-col rounded-2xl bg-card max-h-full overflow-y-auto">
-      <div className="h-[70vh] w-full">
+      <div className="h-full w-full">
         <div className="flex w-full border-b px-4 py-2 justify-between">
           <Skeleton className="h-8 rounded-md px-3 w-[150px]" />
           <Skeleton className="h-8 rounded-md px-3 w-10 border-b" />
         </div>
-        <div className="h-[calc(70vh-50px)] p-6">
-          {Array.from({ length: 8 }).map((_, i) => (
+        <div className="h-full p-6">
+          {Array.from({ length: 12 }).map((_, i) => (
             // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
             <Skeleton className={`w-full ${i % 2 === 0 ? 'h-6' : 'h-9'} mb-4`} key={i} />
           ))}
