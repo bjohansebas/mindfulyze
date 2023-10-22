599abf136e20c312b2978aa1c4f545d804123a24
Sebastian Beltran <bjohansebas@gmail.com>
2023-10-22 13:38:41 -0500

    fix(pricing): add revalidate data

--- src/app/api/plans/route.ts
+++ src/app/api/plans/route.ts
@@ -2,6 +2,8 @@ import { getAllSubscriptionPlans } from '@/lib/api/subscriptionsPlan'
 
 import { NextResponse } from 'next/server'
 
+export const revalidate = 60
+
 export async function GET() {
   const res = await getAllSubscriptionPlans()
 
