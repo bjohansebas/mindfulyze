1c3643732bd40b73422b42aeaa0cdae13194eec5
Sebastian Beltran <bjohansebas@gmail.com>
2023-10-28 15:01:46 -0500

    feat(landing): improve hero section

--- src/app/(mindfulyze)/page.tsx
+++ src/app/(mindfulyze)/page.tsx
@@ -7,7 +7,20 @@ import Templates from '@/components/home/landing/templates'
 export default function Page() {
   return (
     <>
-      <Hero />
+      <header className="max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+        <h1 className="font-extrabold text-4xl sm:text-5xl lg:text-6xl text-center tracking-tighter">
+          Store moments from your day
+          <br />
+          <span className="bg-gradient-to-r from-primary-400 to-primary-600 bg-clip-text text-transparent">
+            Securely
+          </span>
+        </h1>
+        <h2 className="mt-6 text-lg text-slate-300 text-center max-w-2xl mx-auto">
+          Capture, express, and reflect on your day in a secure and private space. Start your journey of self-expression
+          and personal growth
+        </h2>
+        <Hero />
+      </header>
       <SecureData />
       <Templates />
       <SentimentAnalysis />
--- src/components/home/landing/hero.tsx
+++ src/components/home/landing/hero.tsx
@@ -1,23 +1,20 @@
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
+'use client'
+
+import { Button } from '@ui/button'
+
+import { signIn } from 'next-auth/react'
 
 const Hero = () => {
   return (
-    <div className="mx-auto max-w-md px-2.5 text-center sm:max-w-lg sm:px-0 h-[calc(100vh-57px)] flex justify-center flex-col">
-      <h1 className="font-display text-4xl font-bold leading-[1.15] sm:text-6xl sm:leading-[1.15]">
-        Store moments from your day
-        <br />
-        <span className="bg-gradient-to-r from-primary-400 to-primary-600 bg-clip-text text-transparent">Securely</span>
-      </h1>
-      <h2 className="mt-5 text-gray-200 sm:text-xl">
-        Capture, express, and reflect on your day in a secure and private space. Start your journey of self-expression
-        and personal growth
-      </h2>
-      <div className="mx-auto mt-10 flex max-w-fit space-x-4">
-        <Button asChild className="rounded-full">
-          <Link href="/signup">Start For Free</Link>
-        </Button>
-      </div>
+    <div className="flex justify-center mt-6 sm:mt-10">
+      <Button
+        size="lg"
+        onClick={() => {
+          signIn('google', { callbackUrl: '/home' })
+        }}
+      >
+        Start For Free
+      </Button>
     </div>
   )
 }
