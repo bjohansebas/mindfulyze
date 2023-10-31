03ab0fd5809e607744b2804b293e35a092fd895e
Sebastian Beltran <bjohansebas@gmail.com>
2023-10-30 20:38:17 -0500

    feat(landing): improve sentiment-analysis section

--- src/app/(mindfulyze)/page.tsx
+++ src/app/(mindfulyze)/page.tsx
@@ -1,9 +1,10 @@
 import CTA from '@/components/home/landing/cta'
 import Hero from '@/components/home/landing/hero'
 import { SecureDataPreview } from '@/components/home/landing/secure-data'
-import SentimentAnalysis from '@/components/home/landing/sentiment-analysis'
+import { SentimentAnalysis } from '@/components/home/landing/sentiment-analysis'
 import Templates from '@/components/home/landing/templates'
 import { ShieldCheckIcon } from '@heroicons/react/24/solid'
+import { Badge } from '@ui/badge'
 
 export default function Page() {
   return (
@@ -23,18 +24,29 @@ export default function Page() {
         <Hero />
       </header>
       <section className="mt-20 sm:mt-24 mb-20 sm:mb-24  flex md:flex-row flex-col items-center px-8">
-        <div className="flex flex-col gap-6 items-center md:mb-0 mb-10">
+        <header className="flex flex-col gap-6 items-center md:mb-0 mb-10">
           <ShieldCheckIcon className="w-32 h-32" />
           <h2 className=" text-center text-4xl font-bold text-primary-600 sm:text-5xl">Write safely</h2>
           <p className="text-center max-w-3xl mx-auto text-lg">
             Using AES-256 encryption, your personal diary is securely protected. Only you can decrypt and access them,
             ensuring that no one else
           </p>
-        </div>
+        </header>
         <SecureDataPreview />
       </section>
       <Templates />
-      <SentimentAnalysis />
+      <section className="flex items-center flex-col lg:flex-row gap-4">
+        <header className="px-10 w-5/6 lg:w-1/2 flex flex-col gap-6 items-center">
+          <h2 className=" text-center text-4xl font-bold text-primary-600 sm:text-5xl">
+            Sentiment Analysis <Badge>Soon</Badge>
+          </h2>
+          <p className="text-center max-w-3xl mx-auto text-lg">
+            With the help of AI, you will have the ability to experience how your day was or that thought you had during
+            the day.
+          </p>
+        </header>
+        <SentimentAnalysis />
+      </section>
       <CTA />
     </>
   )
--- src/components/home/landing/sentiment-analysis.tsx
+++ src/components/home/landing/sentiment-analysis.tsx
@@ -1,49 +1,21 @@
-import { Badge } from '@/components/ui/badge'
-import Image from 'next/image'
-
-const SentimentAnalysis = () => {
+export function SentimentAnalysis() {
   return (
-    <div className="relative h-[80vh] mx-auto max-w-lg px-2.5 text-center sm:max-w-4xl sm:px-0 flex justify-center flex-col gap-5 items-center">
-      <div>
-        <Image
-          src="/_static/emojis/disappointed_face_3d.png"
-          width="100"
-          height="100"
-          className="absolute left-4 top-3"
-          alt="Disappointed Face"
-        />
-        <Image
-          src="/_static/emojis/frowning_face_3d.png"
-          width="100"
-          height="100"
-          className="absolute right-7 bottom-2"
-          alt="Frowning face"
-        />
-        <Image
-          src="/_static/emojis/grinning_face_3d.png"
-          width="100"
-          height="100"
-          className="absolute left-7 bottom-20"
-          alt="Grinning face"
-        />
-        <Image
-          src="/_static/emojis/slightly_smiling_face_3d.png"
-          width="100"
-          height="100"
-          className="absolute right-1 top-7"
-          alt="Slightly smiling face"
-        />
+    <div className="p-10 lg:w-1/3 lg:space-y-7 grid md:grid-cols-2 md:grid-rows-2 gap-4 lg:block place-items-center">
+      <div className="p-6 py-2 col-span-2 sm:col-span-1 shadow-red-500/30 shadow-lg rounded-xl bg-card/70 backdrop-blur ring-1 ring-inset ring-red-900">
+        Today, I had a car accident. Fortunately, there were no serious injuries, but my vehicle suffered significant
+        damage. The situation was stressful and frustrating, and I&apos;m dealing with bureaucratic procedures for
+        insurance. This incident has had a negative impact on my day and my mood.
       </div>
-
-      <h2 className="font-display text-xl font-bold leading-[1.15] text-primary-600 sm:text-6xl sm:leading-[1.15]">
-        Sentiment Analysis <Badge>Soon</Badge>
-      </h2>
-      <p className="max-w-lg">
-        With the help of AI, you will have the ability to experience how your day was or that thought you had during the
-        day.
-      </p>
+      <div className="p-6 py-2 col-span-2 sm:col-span-1 lg:ml-20 lg:w-[calc(100%+5rem)] shadow-yellow-600/30 shadow-lg rounded-xl bg-card/70 backdrop-blur ring-1 ring-inset ring-yellow-500">
+        While walking in the city, I ran into an old friend I hadn&apos;t seen in years. It was a wonderful surprise. We
+        sat in a café, reminisced about old times, and caught up on our lives. The conversation and companionship were
+        so comforting that I felt extremely positive by the end of the day.
+      </div>
+      <div className="p-6 py-2 col-span-2 lg:ml-10 lg:w-[calc(100%+2.5rem)] shadow-lg shadow-primary-500/30 rounded-xl bg-card/70 backdrop-blur ring-1 ring-inset ring-primary-600">
+        I spent the afternoon taking a walk in the park. The weather was pleasant, and people were enjoying the
+        outdoors. I watched children playing in the park and people walking their dogs. It was a peaceful and relaxing
+        experience, without any significant excitement, just a moment of tranquility in the midst of nature.
+      </div>
     </div>
   )
 }
-
-export default SentimentAnalysis
