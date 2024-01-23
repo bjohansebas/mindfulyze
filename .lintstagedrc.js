0147bd217214bd0592d979f7dfc657b3a6d3de13
Sebastian Beltran <bjohansebas@gmail.com>
2024-01-22 23:21:41 -0500

    chore(turborepo): add lint command to turborepo config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,10 @@
 const path = require('path')
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+  `eslint --fix ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '**/*.{js,jsx,ts,tsx}': [buildEslintCommand],
   '*': [
     'biome check --no-errors-on-unmatched --files-ignore-unknown=true', // Check formatting and lint
   ],
