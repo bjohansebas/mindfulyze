66b3bcc62a518e82dda0de73334ba3008e5cc0ef
Sebastian Beltran <bjohansebas@gmail.com>
2024-01-25 20:45:26 +0000

    fix(ui): 🐛 fix outline button background

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
       variant: {
         default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
         destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
-        outline: 'border border-input bg-popover/50 shadow-sm hover:bg-popover/50 hover:text-accent-foreground',
+        outline: 'border border-input bg-popover/30 shadow-sm hover:bg-popover/60 hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
