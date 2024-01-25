14e0fdefbdaed91d590d7c5410e5bd26b22d8396
Sebastian Beltran <bjohansebas@gmail.com>
2024-01-25 01:13:09 +0000

    feat(ui): ✨ add new button component

---
+++ packages/ui/src/components/Button/Button.tsx
@@ -0,0 +1,56 @@
+import { Slot } from '@radix-ui/react-slot'
+import { type VariantProps, cva } from 'class-variance-authority'
+import * as React from 'react'
+
+import { cn } from '@mindfulyze/utils'
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center gap-3 whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+        destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
+        outline: 'border border-input bg-popover/50 shadow-sm hover:bg-popover/50 hover:text-accent-foreground',
+        secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        link: 'text-primary underline-offset-4 hover:underline',
+        surface: 'border bg-accent text-accent-foreground shadow-sm border-primary hover:bg-accent/80',
+      },
+      size: {
+        tiny: 'h-6 text-xs px-2.5 py-1',
+        sm: 'h-8 text-sm px-3 py-2 leading-4',
+        default: 'h-9 text-sm px-4 py-2',
+        lg: 'h-10 px-4 py-2 text-base',
+      },
+      shape: {
+        square: 'rounded-[8px]',
+        circle: 'rounded-[100%]',
+        rounded: 'rounded-full',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+      shape: 'square',
+    },
+  },
+)
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, shape, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button'
+    return <Comp className={cn(buttonVariants({ variant, size, shape, className }))} ref={ref} {...props} />
+  },
+)
+Button.displayName = 'Button'
+
+export { Button, buttonVariants }
+
+export default Button
---
+++ packages/ui/tsup.config.ts
@@ -0,0 +1,16 @@
+import { Options, defineConfig } from 'tsup'
+
+export default defineConfig((options: Options) => ({
+  entry: ['src/**/*.tsx'],
+  format: ['esm'],
+  esbuildOptions(options) {
+    options.banner = {
+      js: '"use client"',
+    }
+  },
+  dts: true,
+  minify: true,
+  external: ['react'],
+  clean: true,
+  ...options,
+}))
