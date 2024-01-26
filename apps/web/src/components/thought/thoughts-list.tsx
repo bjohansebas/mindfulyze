7f10d3655bae02bd194905cb00a4bc974768c91f
Sebastian Beltran <bjohansebas@gmail.com>
2024-01-26 22:32:13 +0000

    feat(app): 💄 improve style of editor thoughts

---
+++ apps/web/src/components/thought/thoughts-list.tsx
@@ -0,0 +1,21 @@
+import { getThoughts } from '@/lib/api/thoughts'
+import { ThoughtsEmpty } from '../dashboard/thoughts/empty-thoughts'
+import { ThoughtEditor } from './thought-editor'
+
+export async function ThoughtsList() {
+  const thoughtsData = await getThoughts()
+
+  return (
+    <>
+      {thoughtsData.data.length > 0 ? (
+        thoughtsData.data.map(({ id, text, createdAt }) => (
+          <>
+            <ThoughtEditor key={id} text={text} id={id} createdAt={createdAt} />
+          </>
+        ))
+      ) : (
+        <ThoughtsEmpty />
+      )}
+    </>
+  )
+}
--- apps/web/src/components/thought/thoughts-placeholder.tsx
+++ apps/web/src/components/thought/thoughts-placeholder.tsx
@@ -2,16 +2,16 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 export default function ThoughtsPlaceholder() {
   return (
-    <div className="border w-full flex flex-col rounded-2xl bg-card max-h-full overflow-y-auto">
+    <div className="w-full flex flex-col">
       {Array.from({ length: 2 }).map((_, i) => (
         // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-        <div className="h-[70vh] w-full" key={i}>
-          <div className="flex w-full border-b px-4 py-2 justify-between">
-            <Skeleton className="h-8 rounded-md px-3 w-[150px]" />
+        <div className="w-full flex flex-col gap-4" key={i}>
+          <div className="flex w-full py-4 pb-0 justify-between items-center">
+            <Skeleton className="h-6 rounded-md px-3 w-[150px]" />
             <Skeleton className="h-8 rounded-md px-3 w-10 border-b" />
           </div>
-          <div className="h-[calc(70vh-50px)] p-6">
-            {Array.from({ length: 8 }).map((_, i) => (
+          <div className="h-[250px] border border-primary rounded-xl p-6">
+            {Array.from({ length: 5 }).map((_, i) => (
               // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
               <Skeleton className={`w-full ${i % 2 === 0 ? 'h-6' : 'h-9'} mb-4`} key={i} />
             ))}
