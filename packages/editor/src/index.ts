277c5be5f157f563ca9177c09e8758bae0a46347
Sebastian Beltran <bjohansebas@gmail.com>
2024-01-26 04:35:38 +0000

    refactor(editor): 🚚 move some files of editor

--- packages/editor/src/index.ts
+++
@@ -1,6 +0,0 @@
-import './styles/index.css'
-import './styles/prosemirror.css'
-
-export { default as Editor } from './ui'
-export { TiptapExtensions } from './ui/extensions'
-export { TiptapEditorProps } from './ui/props'
