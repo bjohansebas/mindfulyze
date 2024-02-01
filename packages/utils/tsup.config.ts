fe7830c67318551a23faac859440f67b3cacdb95
Sebastian Beltran <bjohansebas@gmail.com>
2024-02-01 23:16:09 +0000

    revert(utils):⏪️ "build(utils): 🔥 remove tsup"
    This reverts commit ccaa8cafbf022ac26b2a8efb1ea6161a43d58a1d.

---
+++ packages/utils/tsup.config.ts
@@ -0,0 +1,10 @@
+import { Options, defineConfig } from 'tsup'
+
+export default defineConfig((options: Options) => ({
+  entry: ['src/**/*.ts'],
+  format: ['esm'],
+  dts: true,
+  minify: true,
+  external: ['react'],
+  ...options,
+}))
