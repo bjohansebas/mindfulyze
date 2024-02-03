452e05ff6076a620a89d728d7ae39374c8f7c6df
Sebastian Beltran <bjohansebas@gmail.com>
2024-02-03 02:51:06 +0000

    feat(app): ✨ add date filter for thoughts

--- apps/web/src/app/(app)/home/page.tsx
+++ apps/web/src/app/(app)/home/page.tsx
@@ -1,4 +1,9 @@
 import { getThoughtsPages } from '@lib/actions/thought'
+
+import { CreateThought } from '@ui/shared/thoughts/create-thoughts'
+import { Skeleton } from '@ui/skeleton'
+import { DateRangeThought } from '@ui/thought/data-range-thoughts'
+
 import Pagination from '@ui/thought/pagination'
 import { ThoughtsList } from '@ui/thought/thoughts-list'
 import ThoughtsPlaceholder from '@ui/thought/thoughts-placeholder'
@@ -10,20 +15,32 @@ export default async function Page({
 }: {
   searchParams?: {
     page?: string
+    fromDate?: string
+    toDate?: string
   }
 }) {
   const currentPage = Number(searchParams?.page) || 1
+  const fromDate = searchParams?.fromDate ?? undefined
+  const toDate = searchParams?.toDate ?? undefined
 
   const totalPages = await getThoughtsPages()
 
   return (
-    <div className="h-screen md:px-12 px-6 pb-6 flex flex-col max-w-full">
-      <Suspense key={currentPage} fallback={<ThoughtsPlaceholder />}>
-        <ThoughtsList currentPage={currentPage} />
-      </Suspense>
-      <div className="mt-10 flex w-full justify-center">
+    <div className="px-6 md:px-0 py-6 flex flex-col max-w-3xl mx-auto">
+      <header className="flex justify-end gap-2 flex-wrap">
+        <Suspense fallback={<Skeleton className="rounded-[8px] w-[161px] h-9" />}>
+          <CreateThought />
+        </Suspense>
+        <DateRangeThought />
+      </header>
+      <section>
+        <Suspense key={`${currentPage}${fromDate}${toDate}`} fallback={<ThoughtsPlaceholder />}>
+          <ThoughtsList currentPage={currentPage} fromDate={fromDate} toDate={toDate} />
+        </Suspense>
+      </section>
+      <footer className="mt-10 flex w-full justify-center">
         <Pagination totalPages={totalPages.data} />
-      </div>
+      </footer>
     </div>
   )
 }
