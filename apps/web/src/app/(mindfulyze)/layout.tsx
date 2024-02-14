50a5889a2371da79e53dafa1f0ff183c721f3d2a
Sebastian Beltran <bjohansebas@gmail.com>
2024-02-13 21:41:05 -0500

    feat(app):  improve landing page

--- apps/web/src/app/(mindfulyze)/layout.tsx
+++ apps/web/src/app/(mindfulyze)/layout.tsx
@@ -1,4 +1,4 @@
-import Footer from '@/components/home/footer'
+import Footer from '@ui/home/footer'
 import Navigation from '@ui/home/nav'
 
 export default function MindfulyzeLayout({
@@ -10,7 +10,7 @@ export default function MindfulyzeLayout({
     <>
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(119,198,183,0.3),rgba(255,255,255,0))]" />
       <Navigation />
-      <main className="pt-3">{children}</main>
+      {children}
       <Footer />
     </>
   )
--- apps/web/src/app/(mindfulyze)/page.tsx
+++ apps/web/src/app/(mindfulyze)/page.tsx
@@ -1,26 +1,33 @@
-import CTA from '@/components/home/landing/cta'
-import Hero from '@/components/home/landing/hero'
-import { ShieldCheckIcon } from '@heroicons/react/24/solid'
-import { LayoutTemplateIcon } from 'lucide-react'
+import Link from 'next/link'
+
+import { Button } from '@mindfulyze/ui'
+import { LogoType } from '@ui/shared/icons'
+import { Bookmarks } from '@ui/shared/illustrations/bookmarks'
+import { Draw } from '@ui/shared/illustrations/draw'
+import { SecureData } from '@ui/shared/illustrations/secure-data'
 
 export default function Page() {
   return (
     <>
-      <header className="max-w-lg md:max-w-3xl mx-auto md:pt-16 pt-10  px-2 md:px-0">
-        <h1 className="font-bold text-[2rem] md:text-6xl text-center text-balance">
+      <header className="max-w-lg md:max-w-3xl mx-auto md:pt-16 pt-10 px-2 md:px-0 mb-20">
+        <h1 className="font-semibold text-[2rem] md:text-6xl text-center text-balance">
           Practice journaling in a <span className="text-primary">safe</span> and{' '}
           <span className="text-primary">simple</span> manner
         </h1>
-        <p className="mt-4 max-w-[26rem] mx-auto md:max-w-xl text-muted-foreground lg:leading-loose text-pretty text-center">
+        <p className="mt-6 max-w-[26rem] mx-auto md:max-w-xl text-muted-foreground lg:leading-loose text-pretty text-center">
           Turn every moment into an opportunity to grow and get to know yourself, exploring your thoughts and ideas in
           one single place.
         </p>
-        <Hero />
+        <div className="flex justify-center mt-6 sm:mt-10">
+          <Button shape="rounded" size="lg" asChild>
+            <Link href="/signup">Start for Free</Link>
+          </Button>
+        </div>
       </header>
-      <div className="mt-20 sm:mt-24 p-10 grid grid-cols-2 grid-rows-2 gap-4 place-items-center">
-        <section className="col-span-2 md:col-span-1">
+      <main className=" max-w-lg md:max-w-5xl mx-auto flex flex-col gap-20 mb-20">
+        <section className="flex items-center gap-3 md:flex-row flex-col justify-center">
+          <SecureData className="w-[80%]" />
           <header className="flex flex-col gap-6 items-center p-5">
-            <LayoutTemplateIcon className="w-32 h-32" />
             <h2 className="text-center text-4xl font-bold text-emerald-600">Create your notes super fast</h2>
             <p className="text-center max-w-3xl mx-auto text-lg">
               With the help of templates, you will have the ease of starting to write a journal and reflect on the
@@ -28,9 +35,23 @@ export default function Page() {
             </p>
           </header>
         </section>
-        <section className="col-span-2 md:col-span-1">
+        <section className="grid grid-cols-2 gap-3 items-center place-items-center">
           <header className="flex flex-col gap-6 items-center p-5">
-            <ShieldCheckIcon className="w-32 h-32" />
+            <h2 className="text-center text-4xl font-bold text-emerald-600">Write safely</h2>
+            <p className="text-center max-w-3xl mx-auto text-lg">
+              Using AES-256 encryption, your personal diary is securely protected. Only you can decrypt and access them,
+              ensuring that no one else
+            </p>
+          </header>
+          <div className="">
+            <Bookmarks className="h-96 text-[#e6e7e8]" />
+          </div>
+        </section>
+        <section className="grid grid-cols-2 gap-3 items-center place-items-center">
+          <div>
+            <Draw className="h-96" />
+          </div>
+          <header className="flex flex-col gap-6 items-center p-5">
             <h2 className="text-center text-4xl font-bold text-emerald-600">Write safely</h2>
             <p className="text-center max-w-3xl mx-auto text-lg">
               Using AES-256 encryption, your personal diary is securely protected. Only you can decrypt and access them,
@@ -38,8 +59,22 @@ export default function Page() {
             </p>
           </header>
         </section>
-      </div>
-      <CTA />
+        <section className="flex justify-center flex-col items-center gap-4">
+          <div>
+            <LogoType className="w-auto h-16 text-emerald-600" />
+          </div>
+          <h2 className="text-4xl font-extrabold leading-tigh sm:text-5xl sm:leading-tight">
+            <span className="bg-gradient-to-r from-emerald-500 to-emerald-700 bg-clip-text text-transparent">
+              Free your thoughts
+            </span>
+            <br />
+            free your mind
+          </h2>
+          <Button asChild>
+            <Link href="/signup">Try Mindfulyze For Free</Link>
+          </Button>
+        </section>
+      </main>
     </>
   )
 }
