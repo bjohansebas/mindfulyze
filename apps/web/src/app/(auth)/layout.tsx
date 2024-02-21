5e22756989fad4ff5ae97a32d846a054498a7211
Sebastian Beltran <bjohansebas@gmail.com>
2024-02-20 21:28:55 -0500

    feat(app): ⚡️ improve background

--- apps/web/src/app/(auth)/layout.tsx
+++ apps/web/src/app/(auth)/layout.tsx
@@ -1,5 +1,3 @@
-import Background from '@/components/background'
-
 export default function AuthLayout({
   children,
 }: {
@@ -7,7 +5,11 @@ export default function AuthLayout({
 }) {
   return (
     <main className="flex h-screen w-screen justify-center items-center">
-      <Background />
+      <div className="fixed left-0 top-0 -z-10 h-svh w-full">
+        <div className="relative h-svh w-full bg-background">
+          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#22282f,transparent)]" />
+        </div>
+      </div>
       {children}
     </main>
   )
--- apps/web/src/app/settings/password/layout.tsx
+++ apps/web/src/app/settings/password/layout.tsx
@@ -1,5 +1,3 @@
-import Background from '@/components/background'
-
 export default function PasswordLayout({
   children,
 }: {
@@ -7,8 +5,12 @@ export default function PasswordLayout({
 }) {
   return (
     <div className="flex min-h-screen flex-col">
+      <div className="fixed left-0 top-0 -z-10 h-svh w-full">
+        <div className="relative h-svh w-full bg-background">
+          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#22282f,transparent)]" />
+        </div>
+      </div>
       <main>{children}</main>
-      <Background />
     </div>
   )
 }
