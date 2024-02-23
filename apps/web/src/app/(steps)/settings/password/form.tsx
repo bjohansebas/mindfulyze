785e2920a0fe81053bf7cf78a215ef0c6ad39ef6
Sebastian Beltran <bjohansebas@gmail.com>
2024-02-22 22:34:16 -0500

    feat(app): ✨ improve fallback of password

--- apps/web/src/app/(steps)/settings/password/form.tsx
+++ apps/web/src/app/(steps)/settings/password/form.tsx
@@ -1,13 +1,13 @@
 import { getUser } from '@/app/actions/user'
-import { NewPasswordForm } from '@/components/settings/password/newPasswordForm'
-import { SetPasswordForm } from '@/components/settings/password/setPasswordForm'
+import { NewPasswordForm } from '@ui/settings/password/newPasswordForm'
+import { SetPasswordForm } from '@ui/settings/password/setPasswordForm'
 import { LockIcon } from 'lucide-react'
 
 export async function PasswordForm() {
   const user = await getUser()
 
   return (
-    <div className="z-10 h-fit w-full max-w-md overflow-hidden border sm:rounded-2xl sm:shadow-xl bg-card">
+    <div className="z-10 h-fit w-full max-w-md overflow-hidden border rounded-2xl sm:shadow-xl bg-card">
       <div className="flex flex-col items-center justify-center space-y-3 px-4 py-3 pt-8 text-center sm:px-16">
         <LockIcon className="h-10 w-10" />
         <h3 className="text-xl font-semibold">
@@ -19,8 +19,8 @@ export async function PasswordForm() {
             : 'Before we begin, first create a password to keep all your thoughts secure.'}
         </p>
       </div>
-      <div className="flex flex-col space-y-3 px-4 py-8 sm:px-16">
-        {user.data?.password != null ? <SetPasswordForm /> : <NewPasswordForm />}
+      <div className="flex flex-col space-y-3 px-4 pb-8 pt-3 sm:px-16">
+        {user.data?.password == null ? <SetPasswordForm /> : <NewPasswordForm />}
       </div>
     </div>
   )
--- apps/web/src/app/(steps)/settings/password/page.tsx
+++ apps/web/src/app/(steps)/settings/password/page.tsx
@@ -1,23 +1,17 @@
-import { Button } from '@/components/ui/button'
 import { constructMetadata } from '@mindfulyze/utils'
 import { PasswordForm } from './form'
 
+import { Skeleton } from '@mindfulyze/ui'
 import { Suspense } from 'react'
 
 export const metadata = constructMetadata({
   title: 'Password | Mindfulyze',
 })
 
-export default function Password() {
+export default function PasswordSettingsPage() {
   return (
     <div className="flex h-screen w-screen justify-center items-center">
-      <Suspense
-        fallback={
-          <div className="flex flex-col space-y-3  px-4 py-8 sm:px-16">
-            <Button disabled={true} variant="secondary" className="w-full" size="lg" />
-          </div>
-        }
-      >
+      <Suspense fallback={<Skeleton className="h-[488px] max-w-md w-full bg-card" />}>
         <PasswordForm />
       </Suspense>
     </div>
