2ea1bd9fec3b35300cf1661d30e8cdaf4a85722c
Sebastian Beltran <bjohansebas@gmail.com>
2024-02-24 15:05:58 -0500

    fix(app): 🐛 check thought data isn't null

--- apps/web/src/components/thought/thoughts-list.tsx
+++ apps/web/src/components/thought/thoughts-list.tsx
@@ -14,7 +14,7 @@ export async function ThoughtsList({
 }) {
   const thoughtsData = await getThoughts({ page: currentPage, toDate, fromDate })
 
-  return thoughtsData.data.length > 0 ? (
+  return thoughtsData.data != null && thoughtsData.data.length > 0 ? (
     thoughtsData.data.map(({ id, text, createdAt }, index) => (
       <article key={id} className="flex gap-x-3 max-w-full">
         <div className="relative last:after:hidden after:absolute after:top-0 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-primary   dark:group-hover:after:bg-primary">
