a8ed439f69544d47d8ffc2ca41a189226229cdb3
Sebastian Beltran <bjohansebas@gmail.com>
2024-02-24 15:30:05 -0500

    feat(ui): improve button component

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -5,7 +5,7 @@ import * as React from 'react'
 import { cn } from '@mindfulyze/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-3 whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center gap-3 whitespace-nowrap text-sm font-medium transition focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -19,9 +19,9 @@ const buttonVariants = cva(
       },
       size: {
         tiny: 'h-6 text-xs px-2.5 py-1',
-        sm: 'h-8 text-sm px-3 py-2 leading-4',
-        default: 'h-9 text-sm px-4 py-2',
-        lg: 'h-10 px-8 py-2 text-base',
+        sm: 'h-8 text-xs px-3 py-2 leading-4',
+        default: 'h-9 px-4 py-2',
+        lg: 'h-10 px-8 py-2',
       },
       shape: {
         square: 'rounded-[8px]',
