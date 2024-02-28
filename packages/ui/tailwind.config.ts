b9e88060128809167069571abb7a81e4aa7b0c2c
Sebastian Beltran <bjohansebas@gmail.com>
2024-02-27 22:01:26 -0500

    chore(ui): 🐛 fix tailwind config

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,10 +1,9 @@
-import sharedConfig from 'config/tailwind/tailwind.config.ts'
+import sharedConfig from '@mindfulyze/config/tailwind/tailwind.config.ts'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: ['./**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/*.{js,ts,jsx,tsx}'],
   presets: [sharedConfig],
-  prefix: 'md',
 }
 
 export default config
