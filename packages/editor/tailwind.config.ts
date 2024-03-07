20c7c022f09809f5fff5041f9e4df5ac722c5e67
Sebastian Beltran <bjohansebas@gmail.com>
2024-03-07 18:41:31 -0500

    chore(editor): update module resolution

--- packages/editor/tailwind.config.ts
+++ packages/editor/tailwind.config.ts
@@ -2,7 +2,7 @@ import sharedConfig from '@mindfulyze/config/tailwind/tailwind.config.ts'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: ['./**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   presets: [sharedConfig],
 }
 
