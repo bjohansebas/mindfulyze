18f1715628b7b452ef9a975c4b17d9136dd034a1
Sebastian Beltran <bjohansebas@gmail.com>
2024-03-07 18:40:39 -0500

    feat(editor): update extensions

--- packages/editor/src/components/editor.tsx
+++ packages/editor/src/components/editor.tsx
@@ -5,10 +5,11 @@ import type { Editor as EditorClass, FocusPosition } from '@tiptap/core'
 import { useDebouncedCallback } from 'use-debounce'
 
 import { generateJSON } from '@tiptap/html'
-import { EditorBubble, EditorContent, EditorRoot } from 'novel'
+import { EditorBubble, EditorCommand, EditorCommandEmpty, EditorCommandItem, EditorContent, EditorRoot } from 'novel'
 import { useState } from 'react'
 
-import { TiptapExtensions } from '../extensions'
+import { extensions } from '../extensions'
+import { suggestionItems } from '../extensions/slash-command'
 import { BackgroundSelector } from './background-selector'
 import { ColorSelector } from './color-selector'
 import { NodeSelector } from './node-selector'
@@ -64,9 +65,10 @@ export function Editor({
   return (
     <EditorRoot>
       <EditorContent
+        editable={editable}
         autofocus={autofocus}
-        initialContent={generateJSON(text, TiptapExtensions)}
-        extensions={TiptapExtensions}
+        initialContent={generateJSON(text, extensions)}
+        extensions={extensions}
         className={cn('bg-card relative min-h-[250px] w-full', className)}
         editorProps={defaultEditorProps}
         onUpdate={({ editor }) => {
@@ -74,6 +76,25 @@ export function Editor({
           debouncedUpdates({ editor })
         }}
       >
+        <EditorCommand className="z-50 h-auto max-h-[330px] w-72 overflow-y-auto rounded-md border border-muted bg-background px-1 py-2 shadow-md transition-all">
+          <EditorCommandEmpty className="px-2 text-muted-foreground">No results</EditorCommandEmpty>
+          {suggestionItems.map((item) => (
+            <EditorCommandItem
+              value={item.title}
+              onCommand={(val) => (item.command != null ? item.command(val) : null)}
+              className="flex w-full items-center space-x-2 rounded-md px-2 py-1 text-left text-sm hover:bg-accent aria-selected:bg-accent "
+              key={item.title}
+            >
+              <div className="flex h-10 w-10 items-center justify-center rounded-md border border-muted bg-background">
+                {item.icon}
+              </div>
+              <div>
+                <p className="font-medium">{item.title}</p>
+                <p className="text-xs text-muted-foreground">{item.description}</p>
+              </div>
+            </EditorCommandItem>
+          ))}
+        </EditorCommand>
         <EditorBubble
           tippyOptions={{
             placement: 'top',
--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -1,7 +1,6 @@
 import './styles/index.css'
-import './styles/prosemirror.css'
 
 export { Editor } from './components/editor'
 
-export { TiptapExtensions } from './extensions'
+export { extensions } from './extensions'
 export { defaultEditorProps } from './components/props'
