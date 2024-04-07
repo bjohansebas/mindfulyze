8e7237fb9796e1f8d819506a545a9dee88f79bfa
Sebastian Beltran <bjohansebas@gmail.com>
2024-04-07 11:30:06 -0500

    feat(app): improve providers

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,20 +1,22 @@
-'use client'
-
+import { auth } from '@lib/auth'
 import { Toaster } from '@mindfulyze/ui'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { SessionProvider } from 'next-auth/react'
 
 type Props = {
   children?: React.ReactNode
 }
 
-export const Provider = ({ children }: Props) => {
+export const Provider = async ({ children }: Props) => {
+  const session = await auth()
+
   return (
-    <>
+    <SessionProvider session={session}>
       <Toaster />
       {children}
       <Analytics />
       <SpeedInsights />
-    </>
+    </SessionProvider>
   )
 }
