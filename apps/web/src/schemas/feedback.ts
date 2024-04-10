570fab9d8052abe4f4b1ad3aa48ca5302ef6696b
Sebastian Beltran <bjohansebas@gmail.com>
2024-04-09 20:53:27 -0500

    refactor(app): remove unused methods

--- apps/web/src/schemas/feedback.ts
+++ apps/web/src/schemas/feedback.ts
@@ -3,7 +3,3 @@ import z from 'zod'
 export const FeedbackSchema = z.object({
   feedback: z.string().min(1),
 })
-
-export function validateFeedback(input: z.infer<typeof FeedbackSchema>) {
-  return FeedbackSchema.safeParse(input)
-}
